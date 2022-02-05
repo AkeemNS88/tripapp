cda4f789e9d7cb6efef24e65b602b191acabf978
Mike <mjpj@live.com>
2022-02-05 18:17:04 -0500

    mapp addded

--- client/src/AddNewTripForm.js
+++ client/src/AddNewTripForm.js
@@ -6,127 +6,126 @@ import React from "react";
 
 function AddNewTripForm({ location, onClose }) {
  
-    const [loading, setLoading] = useState(false);
-    const [formData, setFormData] = useState({
-      title: "",
-      comments: "",
-      rating:"",
-      description: "",
-      image: "",
-      latitude: location.latitude,
-      longitude: location.longitude,
-      visitDate: "",
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    title: "",
+    comments: "",
+    rating:"",
+    description: "",
+    image: "",
+    latitude: location.latitude,
+    longitude: location.longitude,
+    visitDate: "",
+  });
+  console.log("this goes to db",formData)
+  const[ration,setRating]=useState("")
+   
+  const [error, setErros] = useState([]);
+
+  const ratingChanged = (newRating) => {
+    setRating(newRating)
+  };
+
+
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
     });
-    console.log("this goes to db",formData)
-    const[ration,setRating]=useState("")
-     
-    const [error, setErros] = useState([]);
-  
-    const ratingChanged = (newRating) => {
-      setRating(newRating)
-    };
-  
-  
-  
-    const handleChange = (e) => {
-      setFormData({
+  };
+
+  const handleeSubmit = (e) => {
+    e.preventDefault();
+    fetch("/oldtrips", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
         ...formData,
-        [e.target.name]: e.target.value,
-      });
-    };
-  
-    const handleeSubmit = (e) => {
-      e.preventDefault();
-      fetch("/oldtrips", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          ...formData,
-          rating: ration }),
-      }).then((res) => {
-        if (res.ok) {
-          res.json().then((trip) => {
-            setFormData(trip);
-            onClose();
-          });
-        } else {
-          res.json().then((error) => {
-            setErros(error);
-            setLoading(false);
-          });
-        }
-      });
-    };
-  
-    return (
-      <Form onSubmit={handleeSubmit} className="form1">
-        <Form.Group className="mb-3">
-          <Form.Label> Title</Form.Label>
-          <Form.Control
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-          />
-        </Form.Group>
-  
-        <Form.Group className="mb-3">
-          <Form.Label> Commentes</Form.Label>
-          <Form.Control
-            name="comments"
-            row={3}
-            value={formData.comments}
-            onChange={handleChange}
-          />
-        </Form.Group>
-  
-        <ReactStars
-          
-          count={5}
-          onChange={ratingChanged}
-          size={24}
-          isHalf={true}
-          emptyIcon={<i className="far fa-star"></i>}
-          halfIcon={<i className="fa fa-star-half-alt"></i>}
-          fullIcon={<i className="fa fa-star"></i>}
-          activeColor="#ffd700"
+        rating: ration }),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((trip) => {
+          setFormData(trip);
+          onClose();
+        });
+      } else {
+        res.json().then((error) => {
+          setErros(error);
+          setLoading(false);
+        });
+      }
+    });
+  };
+
+  return (
+    <Form onSubmit={handleeSubmit} className="form1">
+      <Form.Group className="mb-3">
+        <Form.Label> Title</Form.Label>
+        <Form.Control
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
         />
-  
-        <Form.Group className="mb-3">
-          <Form.Label> Description</Form.Label>
-          <Form.Control
-            name="description"
-            row={3}
-            value={formData.description}
-            onChange={handleChange}
-          />
-        </Form.Group>
-  
-        <Form.Group className="mb-3">
-          <Form.Label> Image</Form.Label>
-          <Form.Control
-            name="image"
-            value={formData.image}
-            onChange={handleChange}
-          />
-        </Form.Group>
-  
-        <Form.Group className="mb-3">
-          <Form.Label> Visit Date </Form.Label>
-          <Form.Control
-            name="visitDate"
-            type="date"
-            value={formData.visitDate}
-            onChange={handleChange}
-          />
-        </Form.Group>
-  
-        <button id="buttonsubmit1">Submit</button>
-       
-      </Form>
-    );
-  }
-  
-  export default AddNewTripForm;
-  
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label> Commentes</Form.Label>
+        <Form.Control
+          name="comments"
+          row={3}
+          value={formData.comments}
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+      <ReactStars
+        
+        count={5}
+        onChange={ratingChanged}
+        size={24}
+        isHalf={true}
+        emptyIcon={<i className="far fa-star"></i>}
+        halfIcon={<i className="fa fa-star-half-alt"></i>}
+        fullIcon={<i className="fa fa-star"></i>}
+        activeColor="#ffd700"
+      />
+
+      <Form.Group className="mb-3">
+        <Form.Label> Description</Form.Label>
+        <Form.Control
+          name="description"
+          row={3}
+          value={formData.description}
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label> Image</Form.Label>
+        <Form.Control
+          name="image"
+          value={formData.image}
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label> Visit Date </Form.Label>
+        <Form.Control
+          name="visitDate"
+          type="date"
+          value={formData.visitDate}
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+      <button id="buttonsubmit1">Submit</button>
+     
+    </Form>
+  );
+}
+
+export default AddNewTripForm;
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,22 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+   REACT_APP_MAPBOX_KEY = "pk.eyJ1IjoiZGpoeXBlNDEiLCJhIjoiY2t6NHJzcHI4MGpsYzJvcGgwdmlmeWp2OCJ9.I3H41cBWm2dSoHrg6tQf1w"
+
+
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['hype.r.retro@gmail.com'],
+    password:             ENV['Blue0809!,'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
+
 end
